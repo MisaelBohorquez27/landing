30f761abfcbe4a465946223ca3097e634eb8c6f5
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2024-06-05 14:57:27 -0500

    form

--- furni-1.0.0/js/main.js
+++ furni-1.0.0/js/main.js
@@ -5,13 +5,43 @@ let loaded = (eventLoaded) => {
     debugger;
 
 
-    let myform = document.getElementById('<identificador-del-formulario>');
+    let myform = document.getElementById('formulario');
     debugger;
 
     myform.addEventListener('submit', (eventSubmit) => {
+        eventSubmit.preventDefault()
         debugger;
-    })
-}
+        if (form_name.value.length == 0) {
+            alert("Nombre requerido")
+            form_name.focus()
+            return;
+        }
 
-window.addEventListener("DOMContentLoaded", loaded);
+        const nombre = document.getElementById('form-name').value;
+        const apellido = document.getElementById('last-name').value;
+        const email = document.getElementById('email').value;
+        const mensaje = document.getElementById('message').value;
+        const datos = {
+            nombre: nombre,
+            apellido: apellido,
+            email: email,
+            mensaje: mensaje
+        };
+        fetch('https://pokeapi-7a2d2-default-rtdb.firebaseio.com/collection', {
+            method: 'POST',
+            body: JSON.stringify(datos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(respuesta => respuesta.json())
+            .then(datos => {
+                console.log(datos); // Imprimir la respuesta del
+                servidor
+            })
+            .catch(error => console.error(error));
+
+    })
+
+}
 
